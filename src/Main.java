b2ce05dc4c639236ebb7e70070e1e39aa31e2687
akumark8 <akumark8@cisco.com>
2022-12-22 18:56:44 +0530

    committing project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import screens.WelcomeScreen;
+public class Main {
+    public static void main(String[] args) {
+
+        WelcomeScreen welcome = new WelcomeScreen();
+        welcome.introWS();
+        welcome.GetUserInput();
+    }
+}
